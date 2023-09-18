d8b6d212d30edc4f52dd1dc6d552059246dbfc16
Francesc Teruel Rodríguez <francesc.tr03@gmail.com>
2023-09-18 12:33:25 +0200

    Fixed libs

--- EXP_Engine_Sol/ModuleRenderer3D.cpp
+++ EXP_Engine_Sol/ModuleRenderer3D.cpp
@@ -10,9 +10,9 @@
 #pragma comment (lib, "Glew/libx86/glew32.lib")
 
 #ifdef _DEBUG
-#pragma comment (lib, "MathGeoLib/libx86/Debug/MathGeoLib.lib") /* link Microsoft OpenGL lib   */
+#pragma comment (lib, "MathGeoLib/libx86/lib_Debug/MathGeoLib.lib") /* link Microsoft OpenGL lib   */
 #else
-#pragma comment (lib, "MathGeoLib/libx86/Release/MathGeoLib.lib") /* link Microsoft OpenGL lib   */
+#pragma comment (lib, "MathGeoLib/libx86/lib_Release/MathGeoLib.lib") /* link Microsoft OpenGL lib   */
 #endif // _DEBUG
 
 
