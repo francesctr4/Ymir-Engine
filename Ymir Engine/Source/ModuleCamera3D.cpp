3fe2b3ea71552d6d2b6a9bf333d2018938dfb317
Francesc Teruel Rodríguez <francesc.tr03@gmail.com>
2023-11-15 21:38:12 +0100

    Update ModuleCamera3D.cpp

--- Ymir Engine/Source/ModuleCamera3D.cpp
+++ Ymir Engine/Source/ModuleCamera3D.cpp
@@ -25,6 +25,7 @@ ModuleCamera3D::ModuleCamera3D(Application* app, bool start_enabled) : Module(ap
 	editorCamera = new CCamera(nullptr);
 	editorCamera->SetPos(0.0f, 2.0f, 8.0f);
 	editorCamera->LookAt(float3(0.f, 0.f, 0.f));
+	editorCamera->SetAspectRatio(SCREEN_WIDTH/SCREEN_HEIGHT);
 
 	LOG("Creating ModuleCamera3D");
 
